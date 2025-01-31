0d1d2ff7db80e5c9fb7f73410939134a4dc65363
Antonio Almada <38923171+anttonioalmada@users.noreply.github.com>
2025-01-31 09:17:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ var username=document.login.username.value;
 username=username.toLowerCase();
 var password=document.login.password.value;
 password=password.toLowerCase();
-if (username=="123" && password=="123") { window.location="https://www.google.com"; done=1; }
-if (username=="246" && password=="246") { window.location="https://www.google.com"; done=1; }
-if (username=="135" && password=="135") { window.location="https://www.google.com"; done=1; }
+if (username=="123" && password=="123") { window.location.href="https://www.google.com"; done=1; }
+if (username=="246" && password=="246") { window.location.href="https://www.google.com"; done=1; }
+if (username=="135" && password=="135") { window.location.href="https://www.google.com"; done=1; }
 if (done==0) { alert("Login Invalido!"); }
 }
 // End -->
